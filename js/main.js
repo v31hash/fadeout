c2cb23e6beb97ebbbebfe7c18e624a87558b5b03
v31hash <ventures@venture31.com>
2026-01-16 08:07:44 -0500

    Imported wireToggle

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 
-import { loadMovies } from './movies.js';
-import { loadTV } from './tv.js';
+import { loadMovies, wireMovieToggle } from './movies.js';
+import { loadTV, wireTvToggle } from './tv.js';
 
 const API_KEY = 'cb7c7779c5c4232012594c012cf9a701';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const MAX_TRENDS = 6;
 
+
 window.addEventListener('DOMContentLoaded', () => {
-  loadMovies({ apiKey: API_KEY, baseUrl: BASE_URL, max: MAX_TRENDS }); // from file
-  loadTV({ apiKey: API_KEY, baseUrl: BASE_URL, max: MAX_TRENDS });     // from file
-  
-  // const searchBtn = document.querySelector('.search_action');
-  // if (searchBtn) {
-  //   searchBtn.addEventListener('click', () => {
-  //     window.location.href = 'search.html';
-  //   });
-  //   searchBtn.addEventListener('keydown', (e) => {
-  //     if (e.key === 'Enter' || e.key === ' ') {
-  //       e.preventDefault();
-  //       searchBtn.click();
-  //     }
-  //   });
-  // }
+  // Initial loads
+  loadMovies({ apiKey: API_KEY, baseUrl: BASE_URL, max: MAX_TRENDS });
+  loadTV({ apiKey: API_KEY, baseUrl: BASE_URL, max: MAX_TRENDS });
+
+  // Re-load movies whenever the toggle changes
+  wireMovieToggle(() => {
+    loadMovies({ apiKey: API_KEY, baseUrl: BASE_URL, max: MAX_TRENDS });
+  });
+
+  // Re-load movies whenever the toggle changes
+  wireTvToggle(() => {
+    loadTV({ apiKey: API_KEY, baseUrl: BASE_URL, max: MAX_TRENDS });
+  });
+
 });
